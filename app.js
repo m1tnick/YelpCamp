f29fa9337104c6a1ea47bbf670abe0b3517496e1
Vanio <vaniomiguel@gmail.com>
2018-02-13 13:11:43 +0000

    v2

--- app.js
+++ app.js
@@ -1,28 +1,49 @@
-var express = require('express');
-var app = express();
-var bodyParser = require("body-parser");
-
+var express = require('express'),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
+    
+    
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-    { name: "Salmon Creek", image: "https://s3.amazonaws.com/imagescloud/images/cards/camping/camping-tente.jpg" },
-    { name: "Granite Hill", image: "https://img1.sunset.timeinc.net/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/2016/09/main/camping-expert-north-cascade-range-washington-lightweight-tent-0514.jpg?itok=u5IHDgjl" },
-    { name: "Mountain Goat's Rest", image: "https://www.mrmr.biz/wp-content/uploads/2014/09/camping1.jpg" },
-    { name: "Salmon Creek", image: "https://s3.amazonaws.com/imagescloud/images/cards/camping/camping-tente.jpg" },
-    { name: "Granite Hill", image: "https://img1.sunset.timeinc.net/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/2016/09/main/camping-expert-north-cascade-range-washington-lightweight-tent-0514.jpg?itok=u5IHDgjl" },
-    { name: "Salmon Creek", image: "https://s3.amazonaws.com/imagescloud/images/cards/camping/camping-tente.jpg" },
-    { name: "Granite Hill", image: "https://img1.sunset.timeinc.net/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/2016/09/main/camping-expert-north-cascade-range-washington-lightweight-tent-0514.jpg?itok=u5IHDgjl" },
-    { name: "Salmon Creek", image: "https://s3.amazonaws.com/imagescloud/images/cards/camping/camping-tente.jpg" },
-    { name: "Granite Hill", image: "https://img1.sunset.timeinc.net/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/2016/09/main/camping-expert-north-cascade-range-washington-lightweight-tent-0514.jpg?itok=u5IHDgjl" }            
-];
+// Schema Setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//     { 
+//         name: "Granite Hill", 
+//         image: "https://img1.sunset.timeinc.net/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/2016/09/main/camping-expert-north-cascade-range-washington-lightweight-tent-0514.jpg?itok=u5IHDgjl" 
+        
+//     }, function(err, campground) {
+//         if(err) {
+//             console.log(err);
+//         } else {
+//             console.log("NEWLY CREATED CAMPGROUND: ");
+//             console.log(campground);
+//         }
+//     }
+// );
+
 
 app.get("/", function(req, res) {
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res) {
-    res.render("campgrounds", { campgrounds: campgrounds });
+    Campground.find({}, function(err, allCampgrounds) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("campgrounds", { campgrounds: allCampgrounds });        
+        }
+    });
 });
 
 app.post("/campgrounds", function(req, res) {
@@ -31,15 +52,20 @@ app.post("/campgrounds", function(req, res) {
     var image = req.body.image;
 
     var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    // redirect to campgrounds page
-    res.redirect("/campgrounds");
+    Campground.create(newCampground, function(err, newlyCreated) {
+        if(err) {
+            console.log(err);
+        } else {
+            // redirect to campgrounds page
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 app.get("/campgrounds/new", function(req, res){
     res.render("new.ejs");
 });
 
-app.listen(3000, "localhost", function() {
+app.listen(process.env.PORT, process.env.IP, function() {
     console.log("YELPCAMP Server HAS STARTED")
 });
