ee7442a23458dfe9f58b47d0124b95aab95c7122
Vanio <vaniomiguel@gmail.com>
2018-02-14 11:02:10 +0000

    v$

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express'),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
     Campground = require("./models/campground"),
-    //Comment = require("./models/comment"),
+    Comment = require("./models/comment"),
     //User = require("./models/user"),
     seedDB = require("./seeds");
     
@@ -22,7 +22,7 @@ app.get("/campgrounds", function(req, res) {
         if(err) {
             console.log(err);
         } else {
-            res.render("index", { campgrounds: allCampgrounds });        
+            res.render("campgrounds/index", { campgrounds: allCampgrounds });        
         }
     });
 });
@@ -45,7 +45,7 @@ app.post("/campgrounds", function(req, res) {
 });
 
 app.get("/campgrounds/new", function(req, res){
-    res.render("new.ejs");
+    res.render("campgrounds/new");
 });
 
 app.get("/campgrounds/:id", function(req, res) {
@@ -53,10 +53,40 @@ app.get("/campgrounds/:id", function(req, res) {
        if(err) {
            console.log(err);
        } else {
-           res.render("show", {campground: foundCampground});
+           res.render("campgrounds/show", {campground: foundCampground});
        }
     });
 })
+
+// COMMENTS ROUTES
+app.get("/campgrounds/:id/comments/new", function(req, res){
+    Campground.findById(req.params.id, function(err, campground) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("comments/new", {campground: campground})        
+        }
+    });
+})
+
+app.post("/campgrounds/:id/comments", function(req, res){
+    Campground.findById(req.params.id, function(err, campground) {
+        if (err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            Comment.create(req.body.comment, function(err, comment){
+               if(err) {
+                   console.log(err);
+               } else {
+                   campground.comments.push(comment._id);
+                   campground.save();
+                   res.redirect("/campgrounds/"+ campground._id);
+               }
+            });    
+        }
+    }); 
+});
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("YELPCAMP Server HAS STARTED")
