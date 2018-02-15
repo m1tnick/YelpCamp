ebaa27dac25d1d7e4f0a5f18f88287e7a349a549
Vanio <vaniomiguel@gmail.com>
2018-02-15 13:39:46 +0000

    v12

--- routes/comments.js
+++ routes/comments.js
@@ -3,10 +3,11 @@ var router = express.Router({mergeParams: true});
 
 var Campground = require("../models/campground");
 var Comment = require("../models/comment");
+var middleware = require("../middleware");
 
 
 // COMMENTS ROUTES
-router.get("/new", isLoggedIn, function(req, res){
+router.get("/new", middleware.isLoggedIn, function(req, res){
     Campground.findById(req.params.id, function(err, campground) {
         if (err) {
             console.log(err);
@@ -16,9 +17,10 @@ router.get("/new", isLoggedIn, function(req, res){
     });
 })
 
-router.post("/", isLoggedIn, function(req, res){
+router.post("/", middleware.isLoggedIn, function(req, res){
     Campground.findById(req.params.id, function(err, campground) {
         if (err) {
+            req.flash("error", "Something went wrong");
             console.log(err);
             res.redirect("/campgrounds");
         } else {
@@ -32,6 +34,7 @@ router.post("/", isLoggedIn, function(req, res){
                    
                    campground.comments.push(comment);
                    campground.save();
+                   req.flash("success", "Successfully added comment");
                    res.redirect("/campgrounds/"+ campground._id);
                }
             });    
@@ -41,7 +44,7 @@ router.post("/", isLoggedIn, function(req, res){
 
 
 // EDIT
-router.get("/:comment_id/edit", checkCommentOwnership, function(req, res) {
+router.get("/:comment_id/edit", middleware.checkCommentOwnership, function(req, res) {
     Comment.findById(req.params.comment_id, function(err, foundComment) {
         if(err) {
             res.redirect("back");
@@ -53,7 +56,7 @@ router.get("/:comment_id/edit", checkCommentOwnership, function(req, res) {
 
 
 //UPDATE
-router.put("/:comment_id", checkCommentOwnership, function(req, res) {
+router.put("/:comment_id", middleware.checkCommentOwnership, function(req, res) {
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
         if(err) {
             res.redirect("back");
@@ -65,40 +68,17 @@ router.put("/:comment_id", checkCommentOwnership, function(req, res) {
 
 
 // DESTROY
-router.delete("/:comment_id", checkCommentOwnership, function(req, res) {
+router.delete("/:comment_id", middleware.checkCommentOwnership, function(req, res) {
     Comment.findByIdAndRemove(req.params.comment_id, function(err) {
         if(err) {
             console.log(err);
             res.redirect("back");
         } else {
+            req.flash("success", "Comment deleted");
             res.redirect("/campgrounds/"+ req.params.id);        
         }
     });
 });
 
-function checkCommentOwnership(req, res, next) {
-    if(req.isAuthenticated()) {
-        Comment.findById(req.params.comment_id, function(err, foundComment) {
-            if(err) {
-                res.redirect("back");
-            } else {
-                if(foundComment.author.id.equals(req.user._id)) {
-                    next();     
-                } else {
-                    res.redirect("back");
-                }
-            }
-        });
-    } else {
-        res.redirect("back");
-    }
-}
-
-function isLoggedIn(req, res, next) {
-    if(req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect("/login");
-}
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -20,10 +20,12 @@ router.post("/register", function(req, res){
     var newUser = new User({username: req.body.username});
     User.register(newUser, req.body.password, function(err, user) {
        if(err) {
+           req.flash("error", err.message);
            console.log(err);
            return res.render("register");
        } 
        passport.authenticate("local")(req, res, function() {
+           req.flash("success", "Welcome to YelpCamp "+ user.username);
            res.redirect("/campgrounds");
        });
     });
@@ -42,14 +44,9 @@ router.post("/login", passport.authenticate("local",
 
 router.get("/logout", function(req, res) {
     req.logout();
+    req.flash("error", "You have logged out");
     res.redirect("/campgrounds");
 });
 
-function isLoggedIn(req, res, next) {
-    if(req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect("/login");
-}
 
 module.exports = router;
