d197b36ed445e96edbfd45c69e4e91a93664babd
Vanio <vaniomiguel@gmail.com>
2018-02-15 12:58:18 +0000

    v10

--- routes/comments.js
+++ routes/comments.js
@@ -29,7 +29,8 @@ router.post("/", isLoggedIn, function(req, res){
                    comment.author.id = req.user._id;
                    comment.author.username = req.user.username;
                    comment.save();
-                   campground.comments.push(comment._id);
+                   
+                   campground.comments.push(comment);
                    campground.save();
                    res.redirect("/campgrounds/"+ campground._id);
                }
@@ -37,6 +38,61 @@ router.post("/", isLoggedIn, function(req, res){
         }
     }); 
 });
+
+
+// EDIT
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res) {
+    Comment.findById(req.params.comment_id, function(err, foundComment) {
+        if(err) {
+            res.redirect("back");
+        } else {
+            res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});    
+        }
+    });
+});
+
+
+//UPDATE
+router.put("/:comment_id", checkCommentOwnership, function(req, res) {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
+        if(err) {
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
+
+// DESTROY
+router.delete("/:comment_id", checkCommentOwnership, function(req, res) {
+    Comment.findByIdAndRemove(req.params.comment_id, function(err) {
+        if(err) {
+            console.log(err);
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/"+ req.params.id);        
+        }
+    });
+});
+
+function checkCommentOwnership(req, res, next) {
+    if(req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, function(err, foundComment) {
+            if(err) {
+                res.redirect("back");
+            } else {
+                if(foundComment.author.id.equals(req.user._id)) {
+                    next();     
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
+}
 
 function isLoggedIn(req, res, next) {
     if(req.isAuthenticated()) {
